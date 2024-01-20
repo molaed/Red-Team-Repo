d88a2831964488c758f2313d58b9871d4cc78132
kpl9 <kevin_louie_2@sfu.ca>
2024-01-20 00:52:42 -0800

    Did basic routing to Home, About, and Login pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
 import './styles.css'
 import Navbar from './Navbar.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Login from './pages/Login.jsx'
 
 export default function App() {
-
+  let Component
+  switch (window.location.pathname) {
+    case "/":
+      Component = Home
+      break
+    case "/about-us":
+      Component = About
+      break
+    case "/login":
+      Component = Login
+      break
+  }
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Component />
+    </>
   )
 }
