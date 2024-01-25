9a5b92f60f8a3baa0bb9cd929b853fd577bcffc0
thedarianwong <thedarianwong@gmail.com>
2024-01-24 21:06:41 -0800

    Created a Log in Page with Chakra

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+const colors = {
+  brand: {
+    900: '#CC0633',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+const theme = extendTheme({ colors })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme = {theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,56 @@
-export default function Login() {
-    return (
-        <div>Login</div>
-    )
+import React from 'react';
+import {
+  Box,
+  Heading,
+  Input,
+  Button,
+  Select,
+  VStack,
+  useColorModeValue,
+  Flex,
+  Center,
+  AbsoluteCenter,
+} from '@chakra-ui/react';
+
+export default function LoginPage() {
+  return (
+    <Flex
+      align="center"
+      justify="center"
+      mt={20}
+    >
+        <Box bg={useColorModeValue('brand.100', 'brand.700')}
+        p={8}
+        w={['50%', '30%']} // Responsive width
+        mx="auto">
+        <VStack spacing={4} align="flex-start">
+            <Heading as="h1" size="xl" textAlign="center" mb={5}>
+                Log In To SFU Events
+            </Heading>
+
+            <Heading as="h2" size="md" >
+                Email
+            </Heading>
+            <Input placeholder="Email" type="email" />
+            <Heading as="h2" size="md" >
+                Password
+            </Heading>
+            <Input placeholder="Password" type="password" />
+
+            <Heading as="h2" size="md" mt={10}>
+                Authorization
+            </Heading>
+            <Select placeholder="Log in As">
+            <option value="admin">Admin</option>
+            <option value="student">Student</option>
+            </Select>
+
+            <Button colorScheme='red' size="lg" width="full">
+                Log In
+            </Button>
+        </VStack>
+        </Box>
+    </Flex>
+  );
 }
+
